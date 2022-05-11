da0ed25fa9f347ca8a68e6e1a6b55d4574639a96
Frog <artisangiorgio98@gmail.com>
2022-05-11 22:17:31 +0200

    yay

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-const fs = require('fs');  //richiedo il modulo file system
 
-function readCSV() {
-    try {
-        const data = fs.readFileSync('./libri.csv', 'utf8');
-        console.log(data);
-    } catch (err) {
-        throw err;
-    }
-}
-
-try {
-    readCSV()
-} catch (error) {
-    console.log(error.message)
-}
-
-let pippo = {
-    name: 'pippo',
-    dob: 2021
-}
-console.log(pippo.name)
-console.log(pippo['name'])
-pippo['cognome'] = 'de pippis'
-console.log(pippo.cognome)
+// let pippo = {
+//     name: 'pippo',
+//     dob: 2021
+// }
+// console.log(pippo.name)
+// console.log(pippo['name'])
+// pippo['cognome'] = 'de pippis'
+// console.log(pippo.cognome)
 
 //step 1) creare un array di linee
 //  let lines = [[title,author,price,copies],[iliade,omero,10,15],[odissea,omero,15,20],[Dome,S. King,20,5]]
@@ -37,3 +21,41 @@ console.log(pippo.cognome)
 //-aggiungo al nuovo oggetto una proprietà con il nome della proprietà e associo il valore corrispondente nella linea
 //-infilo l'oggetto nell'array vuoto
 //step 5) faccio console.log(arrayOggetti)
+
+
+const fs = require('fs');  //richiedo il modulo file system
+// let data = fs.readFileSync('./names.csv', 'utf8')
+let data = fs.readFileSync(process.argv[2], 'utf8')
+let fileName = process.argv[2]
+
+
+let linesArray = data.split(/\r?\n/)
+const properties = linesArray[0].split(",")
+
+linesArray.shift()
+//WHY libreoffice adds an empty line at the end of the csv file?????
+if(linesArray[linesArray.length-1] === '') linesArray.pop()
+let objectArray = []
+for(let i = 0; i < linesArray.length; i++){
+    const line = linesArray[i].split(",")
+    let bookObject = {}
+    for(let j = 0; j < properties.length; j++){
+        bookObject[properties[j]] = parseInt(line[j]) ? parseInt(line[j]) : line[j]
+    }
+    objectArray.push(bookObject)
+}
+console.log(objectArray)
+
+const jsonFile = JSON.stringify(objectArray)
+const JSONFileName = fileName.split('/').pop().replace('.csv','.json')
+fs.writeFile(JSONFileName, jsonFile, err => {
+    if (err) throw err; 
+    console.log("Done writing"); 
+});
+
+
+
+//namefile.csv = namefile.JSON
+
+
+    
