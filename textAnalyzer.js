dfb5249d7a34c704e6edb1ebc1b263e14ce51507
TheFrog36 <artisangiorgio98@gmail.com>
2022-05-12 13:35:55 +0200

    Update textAnalyzer.js

--- textAnalyzer.js
+++ textAnalyzer.js
@@ -1,10 +1,7 @@
 //1) Importo il fs
-
 //2) Mi leggo gli argomenti inseriti da console
-
-//3) Prendo il primo elemento come input url (obbligatorio) e il secondo come output ulr (non obbligatorio)
-
-//4) Leggo il contenuto del file e loggo il numero di caratteri, spazi compresi e con spazi esclusi
-
+//3) Prendo il primo elemento come input url (obbligatorio) e il secondo come output ulr (obbligatorio) e terza come parola aggiunta
+//4) Leggo il contenuto del file e loggo il numero di caratteri, spazi compresi e con spazi esclusi, il numero di parole e il numero di volte in cui una data parola appare
 //5) Se l'utente ha inserito l'output url, scrivo un nuovo file con il testo dell'originale più i dati dell'analisi
 //   Se l'untente non ha inserito l'url, sovvrascrivo il file originale.
+//5) Controllare quante volte un parola appare
